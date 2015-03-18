9b2147aae5ac5fbb0243b6fd403652c014f8c61b
Hüseyin <hanlamaz13@posta.pau.edu.tr>
2015-03-18 15:32:30 +0200

    Cs dosyaları eklendi

---
+++ InvoiceTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class InvoiceTest
+    {
+        static void Main(string[] args)
+        {
+            Invoice inv1 = new Invoice("1234ab", "Kürek", 25, 9.22M);
+            inv1.ListInvoice();
+        }
+    }
+}
